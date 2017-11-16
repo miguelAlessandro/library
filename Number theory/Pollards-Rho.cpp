a99aec7e3d8eb528d97d58bc880974982bfd28be
miguelAlessandro <mminih@uni.pe>
2017-11-16 00:19:02 -0500

    Update Pollards-Rho.cpp

--- Number theory/Pollards-Rho.cpp
+++ Number theory/Pollards-Rho.cpp
@@ -12,7 +12,7 @@ long long f(long long x, long long c, long long n){
   return add(bimul(x, x, n), c, n);
 }
 
-long long PollardsRho(long long n, int k){
+long long Pollards_Rho(long long n, int k){
  
     if (n==1) return n;
     if (n % 2 == 0) return 2;
@@ -37,3 +37,25 @@ long long PollardsRho(long long n, int k){
         
     return d;
 }
+
+/////////////////////////////////////////////////////
+long long phollards_Rho(long long n){
+  long long x_fixed = 2, x = 2, factor = 1;
+  long long cycle_size = 2;
+	while (factor == 1){
+    
+		for (int i = 1; i <= cycle_size and factor <= 1; ++i){
+			x = add(mul(x, x, n), 1, n);
+			factor = gcd(abs(x - x_fixed), n);
+		}
+
+		cycle_size <<= 1;
+		x_fixed = x;
+	}
+  
+  return factor;
+}
+
+
+
+
