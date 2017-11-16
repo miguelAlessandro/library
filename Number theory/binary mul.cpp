1082a88bec14303a4c7554b9f7ea7fe69f7fd65e
miguelAlessandro <mminih@uni.pe>
2017-11-15 20:06:14 -0500

    Create binary mul.cpp

---
+++ Number theory/binary mul.cpp
@@ -0,0 +1,27 @@
+long long mul(long long a, long long b, long long mod){
+
+  long long r = 0;
+  while(b > 0){
+    if(b&1) r = add(r, a);
+    a = add(a, a);
+    b >>= 1;
+  }
+  return r;
+}
+
+
+long long mul_fast(long long a, long long b, long long mod){
+  a = add(a, 0, mod);
+  b = add(b, 0, mod);
+
+  long long r = 0;
+  while(b > 0){
+    if(b&1) r += a;
+    a += a;
+    
+    if(r >= mod) r -= mod;
+    if(a >= mod) a -= mod;
+    b >>= 1;
+  }
+  return r;
+}
