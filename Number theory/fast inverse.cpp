e5ffdb6612db3ddd9a1c14147b77f34bb45a89c7
miguelAlessandro <mminih@uni.pe>
2017-11-15 20:54:07 -0500

    Create fast inverse.cpp

---
+++ Number theory/fast inverse.cpp
@@ -0,0 +1,21 @@
+/*
+  fast inverse:
+  
+  find x_i in:
+    x_i.i = 1 (mod n) 
+  1 <= i <= m
+  
+  m <= n.
+  complexity: O(m)
+  
+  n%i = n - {n/i}.i
+  inverse{i} = inverse{(n%i)} . {- n/i} [mod n]
+  
+*/
+
+int r[N];
+void fast_inverse(int n, int m){
+  r[1] = 1;
+  for(int i = 2; i <= m; ++i)
+    r[i] = mul(r[n%i], -n/i, n);
+}
