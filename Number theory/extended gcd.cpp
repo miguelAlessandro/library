82b605bcd20d72b2cd77b7574bc4ee14ebe71e6a
miguelAlessandro <mminih@uni.pe>
2017-11-15 19:32:33 -0500

    Create extended gcd.cpp

---
+++ Number theory/extended gcd.cpp
@@ -0,0 +1,19 @@
+int gcd(int a, int b){
+  if(b == 0) return a;
+  return gcd(b, a%b);
+}
+
+// ax + by = gcd(a, b)
+int gcdex(int a, int b, int& x, int &y){
+  if(b == 0){
+    x = 1;
+    y = 0;
+    return a;
+  }
+
+  int x_1, y_1;
+  int g = gcdex(b, a%b, x_1, y_1);
+  x = y_1;
+  y = x_1 - (a/b)*y_1;
+  return g;
+}
