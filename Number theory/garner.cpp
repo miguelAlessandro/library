f27f85a57679339f1d49853b1981cdcfb9640e2b
miguelAlessandro <mminih@uni.pe>
2017-11-15 19:11:18 -0500

    Update garner.cpp

--- Number theory/garner.cpp
+++ Number theory/garner.cpp
@@ -23,29 +23,6 @@ int r[N][N];
 int x[N], a[N], p[N];
 int k;
 
-int add(int a, int b, int mod){
-    a = (a%mod + mod)%mod;
-    b = (b%mod + mod)%mod;
-
-    return (a+b)%mod;
-}
-
-int mul(long long a, long long b, int mod){
-    a = add(a, 0);
-    b = add(b, 0);
-    return a * b % mod;
-}
-
-int expfast(int a, int b, int mod){
-    int r = 1;
-    while(b > 0){
-        if(b&1) r = mul(r, a, mod);
-        a = mul(a, a, mod);
-        b >>= 1;
-    }
-    return r;
-}
-
 void garner_build(int a[], int p[], int k){
 
     for(int i = 0; i < k; ++i)
