3bc60376928efdacf5f86ef6b62976265d65b614
miguelAlessandro <mminih@uni.pe>
2017-11-15 20:18:35 -0500

    Create Miller-Rabin.cpp

---
+++ Number theory/Miller-Rabin.cpp
@@ -0,0 +1,35 @@
+/*
+  Miller-Rabin primality test
+  
+  
+  
+*/
+
+bool Miller_Rabin(long long n, int k){
+  if(n%2 == 0) return n == 2;
+  if(n <= 7) return n != 1; 
+
+  long long d = n-1;
+  int r = 0;
+  
+  while((d&1) == 0){
+    r += 1;
+    d >>= 1;
+  }
+ 
+  while(k--){
+  	x = expfast(rand(n - 3) + 2, d, n); 
+  	if(x == 1 or x == n-1) continue;
+  
+    for(int i = 0; i < r; ++i){
+    	x = bimul(x, x, n);     
+     	
+		if(x == 1) return 0;
+     	if(x == n-1) break;
+     }
+  
+  	if(x != n-1) return 0;
+  }
+
+  return 1;
+}
