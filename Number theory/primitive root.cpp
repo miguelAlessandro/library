4fded99fe518a37518c545482f7edad8dd6842af
miguelAlessandro <mminih@uni.pe>
2017-11-15 22:17:48 -0500

    Update primitive root.cpp

--- Number theory/primitive root.cpp
+++ Number theory/primitive root.cpp
@@ -8,43 +8,21 @@
   
 */
 
-vector<int> prime_factors(int p){
- 
-  if(p == 2 or p == 4)
-    return vector<int>(1, 2);
-   
-  vector<int> ans; 
-  if(p%2 == 0){
-    p >>= 1;
-    
-    if(p%2 == 0) return vector<int>();
-    ans.push_back(2);
-  }
-  
-  for(int i = 3; i*i <= p; i += 2)
-      if(p%i == 0){
-          while(p%i == 0) p /= i;
-          
-          ans.push_back(i);
-      }
-      
-  if(p > 1) ans.push_back(p);
-  if(ans.size() == 1) return ans;
-  if(ans.size() == 2 and ans[0] == 2) return ans;
-  return vector<int>();
-}
-
-
-
 int generator (int p) {
     
-    int Phi = phi(p);
-    vector<int> fact = prime_factors(p);
-
+    int phi = p-1, n = phi;
+    vector<int> fact;
+    for(int i = 2; i*i <= n; ++i)
+      if(n%i == 0){
+        while(n%i == 0) n /= i; 
+        fact.push_back(i);
+      }
+    if(n > 1) ans.push_back(n);
+  
     for (int res = 2; res <= p; ++res){
         bool ok = 1;
         for (int f : fact)
-            ok &= expfast(res, Phi / f, p) != 1;
+            ok &= expfast(res, phi / f, p) != 1;
         
         if (ok)  return res;
     }
