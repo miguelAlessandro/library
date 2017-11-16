4a5f680ec9a5d50d51c97b9c8ba3052fc295ee39
miguelAlessandro <mminih@uni.pe>
2017-11-15 20:21:43 -0500

    Update Miller-Rabin.cpp

--- Number theory/Miller-Rabin.cpp
+++ Number theory/Miller-Rabin.cpp
@@ -1,8 +1,10 @@
 /*
   Miller-Rabin primality test
   
+  solve: 
+      [n is prime]
   
-  
+  O(k . log^2(n)) n <= 10^18 // O(k . log n) n <= 10^9
 */
 
 bool Miller_Rabin(long long n, int k){
@@ -21,12 +23,12 @@ bool Miller_Rabin(long long n, int k){
   	x = expfast(rand(n - 3) + 2, d, n); 
   	if(x == 1 or x == n-1) continue;
   
-    for(int i = 0; i < r; ++i){
-    	x = bimul(x, x, n);     
+    	for(int i = 0; i < r; ++i){
+    		x = bimul(x, x, n);     
      	
 		if(x == 1) return 0;
-     	if(x == n-1) break;
-     }
+     		if(x == n-1) break;
+     	}
   
   	if(x != n-1) return 0;
   }
