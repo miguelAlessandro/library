f44a917c7d687364e5eaa03fdaf6d51ee29fb84c
miguelAlessandro <mminih@uni.pe>
2017-11-15 19:39:55 -0500

    Update inverse.cpp

--- Number theory/inverse.cpp
+++ Number theory/inverse.cpp
@@ -1,9 +1,16 @@
-//asume gcd(a, b) == 1
+/* modular inverse
+    
+    find x in: ax = 1 (mod b)
 
+    asume gcd(a, b) == 1
+*/
+
+//complexity O(log phi(b))
 int invmod(int a, int b){
     return expfast(a, phi(b) - 1, b);
 }
 
+//complexity O(log max(a, b))
 int invmod(int a, int b){
   int x, y;
   int g = gcdex(a, b, x, y);
