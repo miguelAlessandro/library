ccbac174f110412eec6512b4f816521b8fe61115
Fanlat <fanlat@MacBook-Pro-de-Fanlat.local>
2018-04-16 16:50:36 -0500

    from :v mac

---
+++ Computational Geometry/treap.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdio>
+#include <bitset>
+#include <utility>
+
+#include <algorithm>
+#include <ctime>
+#include <cstdlib>
+#include <cstring>
+
+#include <vector>
+#include <string>
+#include <queue>
+#include <stack>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+using namespace std;
+
+int main(){
+
+    
+
+    return 0;
+}
+
