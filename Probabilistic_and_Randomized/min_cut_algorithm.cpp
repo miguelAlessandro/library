eb56c21b17f71d30b9835011a79dcdd7fba956a6
miguelAlessandro <mminih@uni.pe>
2018-07-12 13:55:04 -0500

    cute randomized min-cut algorithm

---
+++ Probabilistic_and_Randomized/min_cut_algorithm.cpp
@@ -0,0 +1,45 @@
+int get (int x) {
+  return x != id[x] ? get(id[x]) : x;
+}
+
+int components;
+void Union(int u, int v) {
+  u = get(u);
+  v = get(v);
+  
+  if (u == v) return;
+  
+  components --;
+  
+  if (rnk[u] < rnk[v]) swap(u, v);
+  id[v] = u;
+  rnk[u] += rnk[u]==rnk[v];
+}
+
+bool same(int u, int v) {
+  return get(u) == get(v);
+}
+
+void init (int V) {
+  for (int i = 1; i <= V; ++i) {
+    id[i] = i;
+    rnk[i] = 0;
+  }
+  components = V;
+}
+
+int min_cut(edge List[], int V, int E) {
+ 
+  init(V);
+  shuffle (List.begin(), List.end());
+  for (edge e : List) {
+    if (components <= 2) break;
+    Union(e.u, e.v);
+  }
+  
+  int cut = 0;
+  for (edge e : List)
+    cut += not same(e.u, e.v);
+  
+  return cut;
+}
